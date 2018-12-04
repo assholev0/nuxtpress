58bf1552ea8d0c342dc341b23dbc4903d13cb021
willin <willin@willin.org>
2018-12-04 16:52:28 +0800

    v1.1.3

--- src/plugins/np.js
+++ src/plugins/np.js
@@ -1,14 +1,11 @@
-export default ({ hotReload, route, app, isStatic }, inject) => {
+export default ({ hotReload, route, app }, inject) => {
   // none biz of context
   if (hotReload || route.fullPath.includes('__webpack_hmr?') || route.fullPath.includes('.hot-update.')) { return; }
 
   const cache = {};
 
-  const fetchContent = async (endpoint, search) => {
+  const fetchContent = async (endpoint, search = '') => {
     const key = endpoint.replace(/(?!^\/)(\/)/g, '.');
-    if (isStatic) {
-      return {};
-    }
     if (!cache[key]) {
       cache[key] = (await app.$axios.get(`/api/${key}${search ? `/${search}` : ''}`)).data;
     }
